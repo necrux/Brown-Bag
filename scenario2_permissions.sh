e3ddfc2953e40121e87b365b4279ce90b3310069
wes <whendersonii@gmail.com>
2015-01-30 00:57:53 -0600

    Added RHEL 6 and 7 support.

--- scenario2_permissions.sh
+++ scenario2_permissions.sh
@@ -6,9 +6,24 @@
 
 version=$(grep -o "release [6-7]" /etc/redhat-release|cut -d' ' -f2)
 
+rm -rf /root/junk/
+
+chown dave:dave /var/www/vhosts/wordpress.*.com/index.php
+chmod 640 /var/www/vhosts/wordpress.*.com/index.php
+
 case $version in
     '6')
+/etc/init.d/httpd restart
+/etc/init.d/mysqld restart
+
+iptables -I INPUT -m conntrack --ctstate NEW -m tcp -p tcp --dport 80 -j ACCEPT
+/etc/init.d/iptables save
 ;;
     '7')
+systemctl restart httpd.service
+systemctl restart mariadb.service
+
+firewall-cmd --zone=public --add-service=http
+firewall-cmd --zone=public --add-service=http --permanent
 ;;
 esac
