ab370d299608e2a2f18fadbe6293da07cc45660a
wes <whendersonii@gmail.com>
2015-01-30 01:14:37 -0600

    Added RHEL 6 and 7 support.

--- scenario3_DBuser.sh
+++ scenario3_DBuser.sh
@@ -6,9 +6,23 @@
 
 version=$(grep -o "release [6-7]" /etc/redhat-release|cut -d' ' -f2)
 
+rm -rf /root/junk/
+
 case $version in
     '6')
+/etc/init.d/httpd restart
+/etc/init.d/mysqld restart
+
+iptables -I INPUT -m conntrack --ctstate NEW -m tcp -p tcp --dport 80 -j ACCEPT
+/etc/init.d/iptables save
 ;;
     '7')
+systemctl restart httpd.service
+systemctl restart mariadb.service
+
+firewall-cmd --zone=public --add-service=http
+firewall-cmd --zone=public --add-service=http --permanent
 ;;
 esac
+
+mysql -e "drop user 'rackspace_user'@'localhost';"
