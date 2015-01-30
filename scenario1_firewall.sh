2cd13e4cd8ce8ded3bd364872d6d52e2c95140a9
wes <whendersonii@gmail.com>
2015-01-29 23:32:29 -0600

    Added RHEL6 support

--- scenario1_firewall.sh
+++ scenario1_firewall.sh
@@ -5,10 +5,30 @@
 #This is due to a firewall rule opening up port 80 being run but not saved.
 
 version=$(grep -o "release [6-7]" /etc/redhat-release|cut -d' ' -f2)
+SSH=$(netstat -pt|awk '/ssh/ {print $4}'|cut -d: -f2)
+
+rm -rf /root/junk/
 
 case $version in
     '6')
+cat > /etc/sysconfig/iptables << EOF
+*filter
+:INPUT ACCEPT [0:0]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+-A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+-A INPUT -p icmp -j ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A INPUT -m conntrack --ctstate NEW -m tcp -p tcp --dport $SSH -j ACCEPT
+-A INPUT -j REJECT --reject-with icmp-host-prohibited
+-A FORWARD -j REJECT --reject-with icmp-host-prohibited
+COMMIT
+EOF
+/etc/init.d/iptables restart
+echo "iptables -I INPUT -m conntrack --ctstate NEW -m tcp -p tcp --dport 80 -j ACCEPT" >> /root/.bash_history
 ;;
     '7')
 ;;
 esac
+
+shutdown -rf now
