8dadff4e0f3032ead9d380b7f3146ef6965d70ca
wes <whendersonii@gmail.com>
2015-02-02 11:33:28 -0600

    Removed TLD restrictions; cleaned out output

--- scripts/initialize_environment.sh
+++ scripts/initialize_environment.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-#NOTE: This script assumes a .com TLD. If you are using a different TLD then you need to amend this file accordingly.
-# sed -i 's/\.com/.<MY_TLD>/g' ./initialize_environment.sh && sed -i 's/cloud\.<MY_TLD>/cloud.com/g' ./initialize_environment.sh
-
 clear
 
 IP=$(ip address show eth0|awk -F'[t /]' '/inet / {print $7}')
 version=$(grep -o "release [6-7]" /etc/redhat-release|cut -d' ' -f2)
 
-echo "Please enter the primary apex/root domain for this project."
-echo -e "   EXAMPLE: http://magento.\e[1;32mlecture\e[0m.com"
-read -p "--> " APEX
+echo "Please enter the primary domain for this project (all project sites will be created as subdomains of this site):"
+echo -e "   EXAMPLE: http://magento.\e[1;32mlecture.com\e[0m"
+read -p "--> " DOMAIN
 
-echo "127.0.0.1 magento.$APEX.com" >> /etc/hosts
+echo "127.0.0.1 magento.$DOMAIN" >> /etc/hosts
 
-sed -i "s/__DOMAIN__/$APEX/g" /root/Brown-Bag/scenario*.sh
-sed -i "s/__DOMAIN__/$APEX/g" /etc/httpd/vhost.d/*.conf
+sed -i "s/__DOMAIN__/$DOMAIN/g" /root/brown-bag/scenario*.sh
+sed -i "s/__DOMAIN__/$DOMAIN/g" /etc/httpd/vhost.d/*.conf
 
-ls /etc/httpd/vhost.d/*__DOMAIN__*|awk -v APEX=$APEX -F'__DOMAIN__' '{print "mv " $1 "__DOMAIN__" $2 " " $1 APEX $2}'|bash
-ls -d /var/www/vhosts/*__DOMAIN__*|awk -v APEX=$APEX -F'__DOMAIN__' '{print "mv " $1 "__DOMAIN__" $2 " " $1 APEX $2}'|bash
+ls /etc/httpd/vhost.d/*__DOMAIN__*|awk -v DOMAIN=$DOMAIN -F'__DOMAIN__' '{print "mv " $1 "__DOMAIN__" $2 " " $1 DOMAIN $2}'|bash
+ls -d /var/www/vhosts/*__DOMAIN__*|awk -v DOMAIN=$DOMAIN -F'__DOMAIN__' '{print "mv " $1 "__DOMAIN__" $2 " " $1 DOMAIN $2}'|bash
 
 read -p "Would you like to automatically add the DNS records to your Rackspace account \
 (this option is only applicable if you are using Rackspace DNS)? [Y/n]: " ANS
@@ -31,15 +28,15 @@ if [ "$ANS" == "y" ] || [ "$ANS" == "Y" ]; then
     TOKEN=$(curl -s -X 'POST' -d "{\"auth\":{\"RAX-KSKEY:apiKeyCredentials\":{\"username\":\"$USERNAME\", \"apiKey\":\"$API\"}}}" \
 -H "Content-Type: application/json" https://identity.api.rackspacecloud.com/v2.0/tokens|python -m json.tool|grep -A5 token|awk -F\" '/id/ {print $4}')
 
-    ID=$(curl -s -H "X-Auth-Token: $TOKEN" https://dns.api.rackspacecloud.com/v1.0/$DDI/domains?name=$APEX.com \
+    ID=$(curl -s -H "X-Auth-Token: $TOKEN" https://dns.api.rackspacecloud.com/v1.0/$DDI/domains?name=$DOMAIN \
 |python -m json.tool|grep id|grep -o [0-9].*[0-9])
 
     curl -s -d '
 {"records": [
-{"name" : "wordpress.'"$APEX"'.com","type" : "A","data" : '\"$IP\"'},
-{"name" : "magento.'"$APEX"'.com","type" : "A","data" : '\"$IP\"'},
-{"name" : "rackspace.'"$APEX"'.com","type" : "A","data" : '\"$IP\"'}
-]}' -H "X-Auth-Token: $TOKEN" -H "Content-Type: application/json" "https://dns.api.rackspacecloud.com/v1.0/$DDI/domains/$ID/records"
+{"name" : "wordpress.'"$DOMAIN"'.com","type" : "A","data" : '\"$IP\"'},
+{"name" : "magento.'"$DOMAIN"'.com","type" : "A","data" : '\"$IP\"'},
+{"name" : "rackspace.'"$DOMAIN"'.com","type" : "A","data" : '\"$IP\"'}
+]}' -H "X-Auth-Token: $TOKEN" -H "Content-Type: application/json" "https://dns.api.rackspacecloud.com/v1.0/$DDI/domains/$ID/records" 2>&1 > /dev/null
 
 else
     echo -e "\nAll files updated. To complete the exercise create the following A records:\n"
@@ -53,19 +50,19 @@ cat << EOF
 
 Be sure to visit the website in your browser to finish configuring the environments:
 
-http://wordpress.$APEX.com
+http://wordpress.$DOMAIN
 
 Database: wordpress
 DB User: wordpress_user
 DB Password: wordpress-lover
 
-http://magento.$APEX.com
+http://magento.$DOMAIN
 
 Database: magento
 DB User: magento_user
 DB Password: magento-lover
 
-http://rackspace.$APEX.com
+http://rackspace.$DOMAIN
 
 Database: rackspace
 DB User: rackspace_user
