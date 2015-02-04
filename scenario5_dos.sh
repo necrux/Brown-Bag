66bc4b8e57287b02422009dc64c899efc656cfdb
wes <whendersonii@gmail.com>
2015-02-04 03:52:54 -0600

    Added solution notes.

--- scenario5_dos.sh
+++ scenario5_dos.sh
@@ -5,6 +5,8 @@
 
 #Solution:
 #This is because the database user is receiving an exesive amount of traffic from a single IP. Be sure to run the /root/brown-bag/scripts/dos.sh script from a different server.
+#Compare page loads time before and after dropping the IP: time curl -I wordpress.__DOMAIN__
+#
 
 version=$(grep -o "release [6-7]" /etc/redhat-release|cut -d' ' -f2)
 
