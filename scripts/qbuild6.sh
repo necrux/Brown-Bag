4a8070ab34b9b67f0716f654158996be9878e881
wes <whendersonii@gmail.com>
2015-02-14 01:38:59 -0600

    Added basic input validation and key generation.

--- scripts/qbuild6.sh
+++ scripts/qbuild6.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
-#Quick build for a 1GB CentOS PVHM image with it the appropriate brown bag cloud-init script.
+#Quick build for a 1GB CentOS 6 PVHM image with it the appropriate brown bag cloud-init script.
 #Script new also be ran remotely via the following command:
 # bash <(curl -sk https://necrux.com/qbuild6.sh)
+clear
+echo -e '        ~~~~BROWN BAG FOR RHEL 6 DEPLOYMENT~~~~\n'
 
 cred_prompt () {
 read -p "SSH User: " SSH_USER
-read -p "Password: " PASSWD
-read -p "SSH Port: " PORT
+read -p "SSH Port [22]: " PORT
+if [ "$PORT" == "" ]; then
+    PORT=22
+else
+    until [ "$PORT" -ge 1 -a "$PORT" -le "65535" ] 2> /dev/null; do
+        echo "$PORT is not a valid entry. Please try again."
+        read -p "SSH Port [22]: " PORT
+    done
+fi
 if [ -f ~/.ssh/id_?sa.pub ]; then
     read -p "SSH public key detected. Would you like to use that key? [Y/n] " ANS
     if [ "$ANS" == "y" ] || [ "$ANS" == "Y" ]; then
         KEY=$(cat ~/.ssh/id_?sa.pub)
+    else
+        read -p "SSH Public Key: " KEY
+        while [ "$KEY" == "" ]; do
+            echo "SSH key required for login. Please enter a valid key."
+            read -p "SSH Public Key: " KEY
+        done
     fi
 else
-    read -p "SSH Public Key: " KEY
+    read -p "No SSH public key detected. Would you like to generate one? [Y/n] " ANS
+    if [ "$ANS" == "y" ] || [ "$ANS" == "Y" ]; then
+        ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N ""
+        KEY=$(cat ~/.ssh/id_rsa.pub)
+    else
+        read -p "SSH Public Key: " KEY
+        while [ "$KEY" == "" ]; do
+            echo "SSH key required for login. Please enter a valid key."
+            read -p "SSH Public Key: " KEY
+        done
+    fi
 fi
 read -p "Mailgun Domain: " DOMAIN
 read -p "Mailgun Password: " MAIL_PASSWD
@@ -24,7 +49,6 @@ read -p "Supnernova Environment Name: " ENV_NAME
 cred_save () {
 cat > ~/.qbuild << EOF
 SSH_USER='$SSH_USER'
-PASSWD='$PASSWD'
 PORT='$PORT'
 KEY='$KEY'
 DOMAIN='$DOMAIN'
@@ -55,7 +79,6 @@ fi
 
 curl -sk https://raw.githubusercontent.com/necrux/cloud-init/master/rhel6_troubleshooting -o /tmp/rhel6_troubleshooting 
 sed -i "s%@USER@%$SSH_USER%g" /tmp/rhel6_troubleshooting
-sed -i "s%@PASSWD@%$PASSWD%g" /tmp/rhel6_troubleshooting
 sed -i "s%@SSH-PORT@%$PORT%g" /tmp/rhel6_troubleshooting
 sed -i "s%@PUB-KEY@%$KEY%g" /tmp/rhel6_troubleshooting
 sed -i "s%@@DOMAIN@%$DOMAIN%g" /tmp/rhel6_troubleshooting
--- scripts/qbuild7.sh
+++ scripts/qbuild7.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-#Quick build for a 1GB CentOS PVHM image with it the appropriate brown bag cloud-init script.
+#Quick build for a 1GB CentOS 7 PVHM image with it the appropriate brown bag cloud-init script.
 #Script new also be ran remotely via the following command:
-# bash <(curl -sk https://necrux.com/qbuild7.sh)
+clear
+echo -e '        ~~~~BROWN BAG FOR RHEL 7 DEPLOYMENT~~~~\n'
 
 cred_prompt () {
 read -p "SSH User: " SSH_USER
-read -p "Password: " PASSWD
-read -p "SSH Port: " PORT
+read -p "SSH Port [22]: " PORT
+if [ "$PORT" == "" ]; then
+    PORT=22
+else
+    until [ "$PORT" -ge 1 -a "$PORT" -le "65535" ] 2> /dev/null; do
+        echo "$PORT is not a valid entry. Please try again."
+        read -p "SSH Port [22]: " PORT
+    done
+fi
 if [ -f ~/.ssh/id_?sa.pub ]; then
     read -p "SSH public key detected. Would you like to use that key? [Y/n] " ANS
     if [ "$ANS" == "y" ] || [ "$ANS" == "Y" ]; then
         KEY=$(cat ~/.ssh/id_?sa.pub)
+    else
+        read -p "SSH Public Key: " KEY
+        while [ "$KEY" == "" ]; do
+            echo "SSH key required for login. Please enter a valid key."
+            read -p "SSH Public Key: " KEY
+        done
     fi
 else
-    read -p "SSH Public Key: " KEY
+    read -p "No SSH public key detected. Would you like to generate one? [Y/n] " ANS
+    if [ "$ANS" == "y" ] || [ "$ANS" == "Y" ]; then
+        ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N ""
+        KEY=$(cat ~/.ssh/id_rsa.pub)
+    else
+        read -p "SSH Public Key: " KEY
+        while [ "$KEY" == "" ]; do
+            echo "SSH key required for login. Please enter a valid key."
+            read -p "SSH Public Key: " KEY
+        done
+    fi
 fi
 read -p "Mailgun Domain: " DOMAIN
 read -p "Mailgun Password: " MAIL_PASSWD
@@ -24,7 +48,6 @@ read -p "Supnernova Environment Name: " ENV_NAME
 cred_save () {
 cat > ~/.qbuild << EOF
 SSH_USER='$SSH_USER'
-PASSWD='$PASSWD'
 PORT='$PORT'
 KEY='$KEY'
 DOMAIN='$DOMAIN'
@@ -54,7 +77,6 @@ fi
 
 curl -sk https://raw.githubusercontent.com/necrux/cloud-init/master/rhel7_troubleshooting -o /tmp/rhel7_troubleshooting 
 sed -i "s%@USER@%$SSH_USER%g" /tmp/rhel7_troubleshooting
-sed -i "s%@PASSWD@%$PASSWD%g" /tmp/rhel7_troubleshooting
 sed -i "s%@SSH-PORT@%$PORT%g" /tmp/rhel7_troubleshooting
 sed -i "s%@PUB-KEY@%$KEY%g" /tmp/rhel7_troubleshooting
 sed -i "s%@@DOMAIN@%$DOMAIN%g" /tmp/rhel7_troubleshooting
